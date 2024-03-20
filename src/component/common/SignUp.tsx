9b4b607103bd5f11a892d9df37a4dd389ecceeb1
MusyafaRT <musyafart@gmail.com>
2024-03-20 21:53:16 +0700

    [STYLE]: add handling error

--- src/component/common/SignUp.tsx
+++ src/component/common/SignUp.tsx
@@ -15,7 +15,9 @@ export default function SignUp() {
     register,
     formState: { errors },
     setError,
+    watch,
   } = useForm<PostAuthSignUpReq>();
+  const password = watch("password", "");
 
   const submitHandler = async (data: PostAuthSignUpReq) => {
     const response = await fetch("/api/auth/user", {
@@ -42,7 +44,7 @@ export default function SignUp() {
         <div className="relative mx-2 mt-8 sm:mx-auto sm:w-full sm:max-w-md rounded-xl bg-cyan">
           <div className="py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <h1 className="text-2xl pt-4 font-medium pb-4">Sign Up</h1>
-            <form className="space-y-6" onSubmit={handleSubmit(submitHandler)}>
+            <form className="space-y-3" onSubmit={handleSubmit(submitHandler)}>
               <div>
                 <label
                   htmlFor="name"
@@ -69,12 +71,22 @@ export default function SignUp() {
                 </label>
                 <div className="mt-1">
                   <input
-                    {...register("email", { required: true })}
+                    {...register("email", {
+                      required: true,
+                      pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    })}
                     name="email"
                     type="text"
                     autoComplete="username"
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${
+                      errors.email ? "border-red-500" : ""
+                    }`}
                   />
+                  {errors.email && (
+                    <p className="text-red-500 text-sm mt-1">
+                      Invalid email address
+                    </p>
+                  )}
                 </div>
               </div>
               <div>
@@ -93,7 +105,9 @@ export default function SignUp() {
                       type={passwordShown ? "text" : "password"}
                       name="password"
                       autoComplete="current-password"
-                      className="block w-full rounded-none rounded-l-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      className={`block w-full rounded-none px-3 rounded-l-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${
+                        errors.password ? "border-red-500" : ""
+                      }`}
                     />
                   </div>
                   {passwordShown ? (
@@ -120,6 +134,65 @@ export default function SignUp() {
                     </button>
                   )}
                 </div>
+                {errors.password && (
+                  <p className="text-red-500 text-sm mt-1">
+                    Password is required
+                  </p>
+                )}
+              </div>
+              <div>
+                <div className="flex justify-between">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Konfirmasi Kata sandi
+                  </label>
+                </div>
+                <div className="mt-1 flex rounded-md shadow-sm">
+                  <div className="relative flex flex-grow items-stretch focus-within:z-10">
+                    <input
+                      {...register("confirmPassword", {
+                        required: true,
+                        validate: (value) => value === password,
+                      })}
+                      type={passwordShown ? "text" : "password"}
+                      name="confirmPassword"
+                      autoComplete="current-password"
+                      className={`block w-full rounded-none px-3 rounded-l-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${
+                        errors.confirmPassword ? "border-red-500" : ""
+                      }`}
+                    />
+                  </div>
+                  {passwordShown ? (
+                    <button
+                      type="button"
+                      onClick={() => setPasswordShown(false)}
+                      className="btn relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:outline-none"
+                    >
+                      <BsEyeFill
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => setPasswordShown(true)}
+                      className="btn relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 "
+                    >
+                      <BsEye
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </button>
+                  )}
+                </div>
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-sm mt-1">
+                    Password is not match
+                  </p>
+                )}
                 <div className="mt-4 flex gap-2">
                   <button
                     type="submit"
--- src/types/api/AuthAPI.ts
+++ src/types/api/AuthAPI.ts
@@ -1,5 +1,6 @@
 export interface PostAuthSignUpReq {
   email: string;
   password: string;
+  confirmPassword: string;
   name: string;
 }
